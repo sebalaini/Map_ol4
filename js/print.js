72baef3199577fc1ede582a83f10bf1402416171
Seba Laini <sebastianolaini@gmail.com>
2018-02-17 20:36:01 +0000

    Aslant

--- js/print.js
+++ js/print.js
@@ -1,25 +1,24 @@
-
 /**
  * Layers.
  */
 
-var mapLayer = new ol.layer.Tile({
+const mapLayer = new ol.layer.Tile({
   source: new ol.source.TileJSON({
     url: 'https://api.tiles.mapbox.com/v3/mapbox.natural-earth-hypso-bathy.json?secure',
     crossOrigin: 'anonymous'
   })
 });
 
-var mainWin = window.opener;
-var mSource = mainWin.mSource;
-var markLayer = mainWin.markLayer;
+const mainWin = window.opener;
+const mSource = mainWin.mSource;
+const markLayer = mainWin.markLayer;
 
 /**
  * Create the map.
  */
 
-var map = new ol.Map({
-  interactions: ol.interaction.defaults({altShiftDragRotate: true, shiftDragZoom: true, mouseWheelZoom: false, pinchZoom: false}).extend([
+const map = new ol.Map({
+  interactions: ol.interaction.defaults({ altShiftDragRotate: true, shiftDragZoom: true, mouseWheelZoom: false, pinchZoom: false }).extend([
       //  drag for overview map
       new ol.interaction.DragRotateAndZoom(),
       // mobile function to force constrainResolution
@@ -39,7 +38,7 @@ var map = new ol.Map({
 
   target: 'map',
   view: new ol.View({
-    center: [0, 0],
+    center: [ 0, 0 ],
     zoom: 3,
     minZoom: 3,
     maxZoom: 8
