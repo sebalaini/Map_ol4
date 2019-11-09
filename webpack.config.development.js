f6ada4d9d3a40bcab6aaa7dc7dc2b6a046ec6e3e
Sebastiano Laini <sebastianolaini@gmail.com>
2019-11-09 16:57:54 +0000

    v2

---
+++ webpack.config.development.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const port = process.env.PORT || 3000
+
+const ROOT_FOLDER = __dirname
+const SRC_FOLDER = path.join(ROOT_FOLDER, 'src')
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+
+  entry: {
+    boundle: [
+      `webpack-hot-middleware/client?path=http://localhost:${port}/__webpack_hmr`,
+      './src/index'
+    ],
+  },
+
+  output: {
+    publicPath: `http://localhost:${port}/`
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    // https://webpack.github.io/docs/hot-module-replacement-with-webpack.html
+    new webpack.HotModuleReplacementPlugin(),
+
+    // “If you are using the CLI, the webpack process will not exit with an error code by enabling this plugin.”
+    // https://github.com/webpack/docs/wiki/list-of-plugins#noerrorsplugin
+    new webpack.NoEmitOnErrorsPlugin(),
+
+    // PARKING WEB
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(SRC_FOLDER, 'index.html'),
+      chunks: ['boundle'],
+    }),
+
+    new CopyWebpackPlugin([
+      // CLIENT CSS
+      {
+        from: 'src/css',
+        to: 'css',
+        toType: 'dir'
+      },
+    ])
+  ],
+})
---
+++ webpack.config.production.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  // devtool: 'cheap-module-source-map',
+
+  entry: {
+    boundle: [
+      '@babel/polyfill',
+      './src/traffweb'
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+  },
+
+  plugins: [
+    // NODE_ENV should be production so that modules do not perform certain development checks
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+  ],
+
+  optimization: {
+    minimizer: [
+      // minify the JS bundle
+      new TerserPlugin(),
+      // minify the CSS bundle
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+})
